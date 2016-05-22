d442af0ebb5b753714aad71d859dac2dff5e7f2d
Ben <codeandcats@gmail.com>
2016-05-22 18:24:43 +1000

    Handles NaN, Infinity, null and undefined properly now

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 (function(root) {
+	
 	if (typeof exports !== 'undefined') {
 		if (typeof module !== 'undefined' && module && module.exports) {
 			exports = module.exports = clone;
@@ -12,36 +13,77 @@
 	}
 	
 	function clone(value) {
-		if (typeof value === 'object') {
-			var result;
-			if (value instanceof Date) {
-				result = new Date();
-				result.setTime(value.getTime());
+		var type = typeof value;
+		switch (type) {
+			case 'object':
+				// null and undefined
+				if (value == null) {
+					return value;
+				}
+				
+				var result;
+				
+				if (value instanceof Date) {
+					result = new Date();
+					result.setTime(value.getTime());
+					return result;
+				}
+				
+				result = JSON.parse(JSON.stringify(value));
+				fixTypes(value, result);
 				return result;
-			}
-			result = JSON.parse(JSON.stringify(value));
-			fixDates(result, value);
-			return result;
-		}
-		else {
-			return value;
+			
+			default:
+				return value;
 		}
 	}
-
-	function fixDates(copy, original) {
-		for (var key in original) {
-			var originalValue = original[key];
-			if (typeof originalValue === 'object') {
+	
+	function fixPropertyValue(original, copy, key) {
+		var originalValue = original[key];
+		var originalType = typeof originalValue;
+		
+		switch (originalType) {
+			case 'object':
 				if (originalValue instanceof Date) {
 					var newValue = new Date();
 					newValue.setTime(originalValue.getTime());
 					copy[key] = newValue;
 				}
-				else {
-					fixDates(copy[key], originalValue);
+				else if (originalValue == null) {
+					copy[key] = originalValue;
 				}
+				else {
+					fixTypes(originalValue, copy[key]);
+				}
+				break;
+			
+			case 'number':
+				if (isNaN(originalValue)) {
+					copy[key] = NaN;
+				}
+				else if (originalValue == Infinity) {
+					copy[key] = Infinity;
+				}
+				break;
+				
+			default:
+				break;
+		}
+	}
+	
+	function fixTypes(original, copy) {
+		if (original instanceof Array) {
+			for (var index = 0; index < original.length; index++) {
+				fixPropertyValue(original, copy, index);
 			}
 		}
-	}	
+		else {
+			var keys = Object.getOwnPropertyNames(original);
+			keys.forEach(function(key) {
+				fixPropertyValue(original, copy, key);
+			});
+		}
+	}
+		
 })(this);
 
