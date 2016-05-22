244db5da87a33319e8b484c14dcaae2042ca54ee
Ben <codeandcats@gmail.com>
2016-05-22 18:27:04 +1000

    Added benchmarking

---
+++ benchmark/dataHelper.js
@@ -0,0 +1,61 @@
+var faker = require('faker');
+var path = require('path');
+var os = require('os');
+var fs = require('fs');
+
+function randomInt(min, max) {
+	return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+var MS_IN_YEAR = 1000 * 60 * 60 * 24 * 365;
+
+function randomPerson(options) {
+	var opts = options || {};
+	
+	opts.maxDateOfBirth = opts.maxDateOfBirth || new Date(Date.now() - MS_IN_YEAR);
+	opts.minAge = opts.minAge || 18;
+	opts.maxAge = opts.maxAge || 105;
+	opts.depth = opts.depth || 0; 
+	
+	// Generates a family tree
+	var result = {
+		firstName: faker.name.lastName(),
+		lastName: faker.name.firstName(),
+		
+		employment: {
+			companyName: faker.company.companyName()  
+		},
+		
+		age: faker.random.number(),
+		dateOfBirth: faker.date.past(),
+		enroledToVote: faker.random.boolean(),
+	};
+	
+	if (opts.depth > 0) {
+		result.parents = [
+			randomPerson({ depth: opts.depth - 1 }),
+			randomPerson({ depth: opts.depth - 1 })
+		];
+	}
+	
+	return result;
+}
+
+module.exports.randomPerson = randomPerson;
+
+function getTempFileName() {
+	return path.join(os.tmpdir(), Math.floor((Math.random() * 10000000)).toString() + '.yo.tmp');
+}
+
+module.exports.jsonSize = function(obj, callback) {
+	var tempFileName = getTempFileName();
+	var text = JSON.stringify(obj, null, '\t');
+	
+	fs.writeFile(tempFileName, text, { encoding: 'utf8' }, function(err) {
+		fs.stat(tempFileName, function(err, stats) {
+			fs.unlink(tempFileName, function() {
+				callback(stats.size);
+			});
+		});
+	});
+}
---
+++ benchmark/index.js
@@ -0,0 +1,112 @@
+var dataHelper = require('./dataHelper');
+var filesize = require('filesize');
+var chalk = require('chalk');
+var pad = require('pad');
+var numeral = require('numeral');
+
+// Various cloning libraries
+var libs = {
+	clone: require('clone'),
+	deepClone: require('deepclone'),
+	'lodash.cloneDeep': require('lodash').cloneDeep,
+	snapshot: (function(snapshot) {
+		return function(data) {
+			var expr = snapshot(data);
+			return eval(expr);
+		};
+	})(require('snapshot')),
+	'fast-clone': require('../')
+};
+
+console.log('Generating random json data...');
+
+var data = dataHelper.randomPerson({ depth: 11 });
+
+console.log('Done.');
+console.log('');
+
+dataHelper.jsonSize(data, function(size) {
+	console.log('Test data size is: ', filesize(size));
+	console.log('');
+	
+	runBenchmarks(libs);
+});
+
+function runBenchmarks(libs) {
+	var names = Object.getOwnPropertyNames(libs);
+	
+	var results = [];
+	
+	names.forEach(function(name, index) {
+		var text = 'Running benchmark ' + (index + 1) + ' of ' + names.length + '...';
+		
+		process.stdout.write(text + '\r');
+		
+		var clone = libs[name];
+		
+		var startTime = Date.now();
+		var maxDuration = 2000;
+		var maxTime = startTime + maxDuration;
+		var endTime = maxTime;
+		var iterations = 0;
+		
+		while (Date.now() < maxTime) {
+			var copy = clone(data);
+			iterations++;
+			endTime = Date.now();
+		}
+		
+		var avgTime = Math.round((endTime - startTime) / iterations);
+		
+		results.push({
+			name: name,
+			time: avgTime 
+		});
+		
+		console.log(text + ' Done');
+	});
+	
+	console.log('');
+	
+	// Sort by time desc
+	results.sort(function(a, b) {
+		return b.time - a.time;
+	});
+	
+	// Change results to array of values and format time
+	results = results.map(function(item) {
+		return [
+			item.name,
+			numeral(item.time).format('0,0') + ' ms'
+		];
+	});
+	
+	var columnTitles = ['Library', 'Time']
+	
+	// Get max column lengths so we can pad them nicely
+	var columnLengths = results.reduce(function(prev, curr) {
+		return [
+			Math.max(prev[0], curr[0].length),
+			Math.max(prev[1], curr[1].length)
+		];
+	}, [columnTitles[0].length, columnTitles[1].length]);
+	
+	// Print the results
+	console.log(
+		pad(columnTitles[0], columnLengths[0]) + 
+		' | ' +
+		pad(columnTitles[1], columnLengths[1]));
+	
+	console.log(
+		pad('', columnLengths[0], '=') +
+		'=|=' + 
+		pad('', columnLengths[1], '='));
+	
+	results.forEach(function(result, index) {
+		var color = index == names.length - 1 ? chalk.green : chalk.reset;
+		console.log(
+			color(pad(result[0], columnLengths[0])) + 
+			' | ' +
+			color(pad(columnLengths[1], result[1])));
+	});
+}
